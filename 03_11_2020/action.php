d8796ee8f6a2ea484f038ad414a381a595f24441
sia041081 <stolajruga@yandex.ru>
2020-11-03 15:30:43 +0300

    Correction

--- 03_11_2020/action.php
+++ 03_11_2020/action.php
@@ -1,7 +1,7 @@
 <?php
 require_once('function.php');
 $str = $_POST['name'];
-if (isset($_POST["rem_spaces"])) {
+if ($_POST['task'] == "rem_spaces") {
     echo "<center><h4>Задача №1</h4></center> В строке текста записаны слова, разделенные пробелами
           в произвольном количестве. Сжатие текста состоит в том, что между
           словами оставляется по одному пробелу, а после последнего слова пробелы
@@ -11,26 +11,27 @@ if (isset($_POST["rem_spaces"])) {
     echo '<br>';
     rem_spaces($str);
 }
-if (isset($_POST["find_word"])) {
+if ($_POST['task'] == "find_word") {
     echo "<center><h4>Задача №2</h4></center> В произвольном тексте найти и вывести на экран все слова
           длина которых не менее трёх символов и не более пяти.";
     echo '<br>';
     find_word($str);
 }
-if (isset($_POST["palindrom"])) {
+if ($_POST["task"] == "palindrom") {
+    print_r($_POST);
     echo "<center><h4>Задача №3</h4></center> Вводится строка. Удалить из нее все пробелы.
           После этого определить, является ли она палиндромом (перевертышем),
           т.е. одинаково пишется как с начала, так и с конца.";
     echo '<br>';
     palindrom($str);
 }
-if (isset($_POST["long_word"])) {
+if ($_POST["task"] == "long_word") {
     echo "<center><h4>Задача №4</h4></center> Найти самое длинное слово и вывести его на экран.
           Случай, когда самых длинных слов может быть несколько, не обрабатывать.";
     echo '<br>';
     long_word($str);
 }
-if (isset($_POST["upper_char"])) {
+if ($_POST["task"] == "upper_char") {
     echo "<center><h4>Задача №5</h4></center> Посчитать количество строчных и прописных букв в строке";
     echo '<br>';
     upper_char($str);
